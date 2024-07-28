581ffa7849d03f71eb6b13fe2426fdfadbc8280b
Adrien Lucbert <adrien.lucbert@gmail.com>
2024-07-28 17:24:16 +0200

    build: use SQLITE_DB_PATH as static env var

--- trackme/src/lib/orm/data-sources/sqlite.ts
+++ trackme/src/lib/orm/data-sources/sqlite.ts
@@ -1,11 +1,11 @@
 import { LiveTrackSession } from '$lib/orm/models/LiveTrackSession'
 import { DataSource } from 'typeorm'
-import { env } from '$env/dynamic/private'
+import { SQLITE_DB_PATH } from '$env/static/private'
 import { dev } from '$app/environment'
 
 export const SqliteDatabase = new DataSource({
   type: 'sqlite',
-  database: env.SQLITE_DB_PATH || '/data/sqlite.db',
+  database: SQLITE_DB_PATH || '/data/sqlite.db',
   entities: [ LiveTrackSession ],
   logging: dev,
 	synchronize: true,
