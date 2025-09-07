ee891c1a2b8b0c49c761aed54aeb4ee77ff4c658
Adrien Lucbert <adrien.lucbert@gmail.com>
2025-09-08 00:53:51 +0200

    feat(ui): responsive title

--- trackme/src/routes/+layout.svelte
+++ trackme/src/routes/+layout.svelte
@@ -1,24 +1,32 @@
 <script lang="ts">
-	import '../app.css'
+	import '../app.css';
 
-	import { navigating } from "$app/stores"
-	import { expoOut } from "svelte/easing"
-	import { slide } from "svelte/transition"
-	import { DarkMode } from 'flowbite-svelte'
-	import { UserCircleSolid, MoonSolid, SunSolid } from 'flowbite-svelte-icons'
+	import { navigating } from '$app/stores';
+	import { expoOut } from 'svelte/easing';
+	import { slide } from 'svelte/transition';
+	import { DarkMode } from 'flowbite-svelte';
+	import { UserCircleSolid, MoonSolid, SunSolid } from 'flowbite-svelte-icons';
 </script>
 
 <div class="bg-white dark:bg-neutral-800 flex items-center">
 	<div class="flex items-center p-2 min-h-16 container mx-auto">
 		<div class="flex-1">
-			<a class="font-semibold text-xl px-4" href="/">Garmin Persistent Livetrack</a>
+			<a class="font-semibold text-xl px-4" href="/">
+				<span class="hidden sm:inline">Garmin Persistent </span>Livetrack
+			</a>
 		</div>
 		<div class="flex-none">
-			<ul class="p-2 flex-wrap inline-flex flex-row space-x-1 font-bold text-lg ">
-				<a href="/auth" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none rounded-lg text-sm p-2.5">
-					<UserCircleSolid size="lg"/>
+			<ul class="p-2 flex-wrap inline-flex flex-row space-x-1 font-bold text-lg">
+				<a
+					href="/auth"
+					class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none rounded-lg text-sm p-2.5"
+				>
+					<UserCircleSolid size="lg" />
 				</a>
-				<DarkMode size="lg" btnClass="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xl p-2">
+				<DarkMode
+					size="lg"
+					btnClass="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xl p-2"
+				>
 					<MoonSolid size="lg" slot="darkIcon" />
 					<SunSolid size="lg" slot="lightIcon" />
 				</DarkMode>
@@ -36,10 +44,9 @@
 	-->
 	<div
 		class="fixed w-full top-0 right-0 left-0 h-1 z-50 bg-primary-700"
-		in:slide={{ delay: 100, duration: 12000, axis: "x", easing: expoOut }}
+		in:slide={{ delay: 100, duration: 12000, axis: 'x', easing: expoOut }}
 	></div>
 {/if}
-
 
 <div class="">
 	<slot />
@@ -54,12 +61,16 @@
 			<span class="mb-2 font-bold uppercase opacity-80">Explore</span>
 			<a class="link mb-1" href="#">How it works</a>
 			<a class="link mb-1" href="#">Self-Host</a>
-			<a class="link my-1" href="https://github.com/adrienlucbert/garmin-persistent-livetrack/">Github</a>
+			<a class="link my-1" href="https://github.com/adrienlucbert/garmin-persistent-livetrack/"
+				>Github</a
+			>
 		</nav>
 		<aside class="grid place-items-start gap-2">
 			<span class="mb-2 font-bold uppercase opacity-80">Notice</span>
 			<span class="max-w-[260px]">
-				This service is in no way related to <a class="link" href="https://www.garmin.com/">Garmin&reg;</a> but extends and simplifies the use of its LiveTrack feature.
+				This service is in no way related to <a class="link" href="https://www.garmin.com/"
+					>Garmin&reg;</a
+				> but extends and simplifies the use of its LiveTrack feature.
 			</span>
 		</aside>
 	</footer>
