e77936fef2b3288a4a7ee14bea1eee97920d99a0
Adrien Lucbert <adrien.lucbert@gmail.com>
2025-09-10 10:49:16 +0200

    feat(ui): reproduce home page with shadcn

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,12 +1,80 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	
-	let { children } = $props();
+	import SunIcon from '@lucide/svelte/icons/sun';
+	import MoonIcon from '@lucide/svelte/icons/moon';
+	import CircleUserRound from '@lucide/svelte/icons/circle-user-round';
+
+	import { ModeWatcher } from 'mode-watcher';
+	import { navigating } from '$app/stores';
+	import { expoOut } from 'svelte/easing';
+	import { slide } from 'svelte/transition';
+	import { toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<ModeWatcher />
 
-{@render children?.()}
+<div class="flex items-center bg-white dark:bg-neutral-800">
+	<div class="container mx-auto flex min-h-16 items-center p-2">
+		<div class="flex-1">
+			<a class="px-4 text-xl font-semibold no-underline" href="/">
+				<span class="hidden sm:inline">Garmin Persistent </span>Livetrack
+			</a>
+		</div>
+		<div class="flex-none">
+			<ul class="inline-flex flex-row flex-wrap space-x-1 p-2 text-lg font-bold">
+				<Button variant="ghost" size="icon" href="/auth/signin">
+					<CircleUserRound />
+				</Button>
+				<Button onclick={toggleMode} variant="ghost" size="icon">
+					<SunIcon class="scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90" />
+					<MoonIcon
+						class="absolute scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+					/>
+					<span class="sr-only">Toggle theme</span>
+				</Button>
+			</ul>
+		</div>
+	</div>
+</div>
+
+{#if $navigating}
+	<!--
+		Loading animation for next page since svelte doesn't show any indicator.
+		 - delay 100ms because most page loads are instant, and we don't want to flash
+		 - long 12s duration because we don't actually know how long it will take
+		 - exponential easing so fast loads (>100ms and <1s) still see enough progress,
+			 while slow networks see it moving for a full 12 seconds
+	-->
+	<div
+		class="bg-primary-700 fixed top-0 right-0 left-0 z-50 h-1 w-full"
+		in:slide={{ delay: 100, duration: 12000, axis: 'x', easing: expoOut }}
+	></div>
+{/if}
+
+<div class="">
+	<slot />
+</div>
+
+<div class="flex-grow"></div>
+<div class="">
+	<footer
+		class="grid w-full grid-flow-row place-content-center place-items-start gap-x-48 gap-y-8 bg-white p-10 text-base sm:grid-flow-col sm:gap-y-10 lg:gap-x-64 xl:gap-x-96 dark:bg-neutral-800"
+	>
+		<nav class="grid place-items-start gap-2">
+			<span class="mb-2 font-bold uppercase opacity-80">Explore</span>
+			<a class="mb-1" href="#">How it works</a>
+			<a class="mb-1" href="#">Self-Host</a>
+			<a class="my-1" href="https://github.com/adrienlucbert/garmin-persistent-livetrack/">Github</a
+			>
+		</nav>
+		<aside class="grid place-items-start gap-2">
+			<span class="mb-2 font-bold uppercase opacity-80">Notice</span>
+			<span class="max-w-[260px]">
+				This service is in no way related to <a class="link" href="https://www.garmin.com/"
+					>Garmin&reg;</a
+				> but extends and simplifies the use of its LiveTrack feature.
+			</span>
+		</aside>
+	</footer>
+</div>
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,42 +1,33 @@
-<script lang="ts">
-	let { data } = $props();
-	let { flags } = data;
-
-	let success = $state(false);
-	let message = $state('');
-
-	async function askVerify(e: SubmitEvent) {
-		e.preventDefault();
-		const formData = new FormData(e.target as HTMLFormElement);
-
-		const res = await fetch('/auth/verify', {
-			method: 'POST',
-			body: formData
-		});
-		success = res.status >= 200 && res.status < 300;
-		const data = await res.json();
-		message = data.message;
-	}
+<script>
+	import { Button } from '$lib/components/ui/button';
 </script>
 
-{#if data.user.passwordTrait}
-	<h1>Hi, {data.user.passwordTrait.email}!</h1>
-{:else if data.user.googleTrait}
-	<h1>Hi, {data.user.googleTrait.username}!</h1>
-{:else if data.user.githubTrait}
-	<h1>Hi, {data.user.githubTrait.username}!</h1>
-{/if}
-<p>Your user ID is {data.user.uuid}.</p>
-{#if flags.ENABLE_VERIFY_EMAIL}
-	{#if data.user.passwordTrait && !data.user.passwordTrait.isEmailVerified}
-		{#if !success}
-			<form onsubmit={askVerify}>
-				<button>Verify email</button>
-			</form>
-		{/if}
-		{message}
-	{/if}
-{/if}
-<form method="post" action="/auth/signout">
-	<button>Sign out</button>
-</form>
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="" />
+</svelte:head>
+
+<div class="grid min-h-[60vh] w-full place-items-center">
+	<div class="flex max-w-[80rem] items-center justify-center gap-4 p-4 py-12 text-center">
+		<div class="max-w-xl">
+			<div class="px-2 py-8 text-4xl font-bold md:text-6xl" style="line-height: 1.2;">
+				<span class="title-highlight">
+					<span class="text-primary">One</span>
+					Tracking Link
+				</span>
+				Share, Notify, Ride
+				<span class="title-highlight"> Worry-Free </span>
+				🚴
+			</div>
+			<div class="mt-6 text-sm md:mt-10 md:text-lg">
+				Transform your <b>ever-changing</b> Garmin Live-Tracking link into a <b>persistent</b> URL
+				and ensure <b>continuous updates</b> with built-in activity start notifications for your
+				<b>friends and family</b>.
+			</div>
+			<div class="mt-6 flex justify-center gap-4 md:mt-4">
+				<Button>Create my link</Button>
+				<Button variant="outline">How it works</Button>
+			</div>
+		</div>
+	</div>
+</div>
