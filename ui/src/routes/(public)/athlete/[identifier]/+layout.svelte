7857ec69e0cd06a1ee41e22a625f819e397b8968
Adrien Lucbert <adrien.lucbert@gmail.com>
2025-12-05 15:46:56 +0100

    feat(ui): toast if push notifications not setup

--- ui/src/routes/(public)/athlete/[identifier]/+layout.svelte
+++ ui/src/routes/(public)/athlete/[identifier]/+layout.svelte
@@ -4,6 +4,9 @@
 	import { type HeaderContext } from '$lib/types/contexts/header.js';
 	import { Button } from '$lib/components/ui/button';
 	import { m } from '$lib/paraglide/messages.js';
+	import { toast } from 'svelte-sonner';
+	import { goto } from '$app/navigation';
+	import { pages } from '$lib/pages.svelte.js';
 
 	let { data, children } = $props();
 	const { trackingLink, follow, user } = data;
@@ -12,6 +15,21 @@
 
 	onMount(() => {
 		header?.addAction('info', infoHeaderAction);
+
+		if (
+			follow &&
+			user?.notificationPreferences.new_livetrack.push &&
+			Notification.permission !== 'granted'
+		) {
+			toast.warning(m.push_notifications_not_setup_title(), {
+				description: m.push_notifications_not_setup_body({ tab: pages().account.title }),
+				duration: 15000,
+				action: {
+					label: m.push_notifications_not_setup_button(),
+					onClick: () => goto('/account#notifications')
+				}
+			});
+		}
 	});
 
 	onDestroy(() => {
